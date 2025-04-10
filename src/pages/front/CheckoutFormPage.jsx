d54ffd8204ca2ba00df4cd2e4d188f59934b553b
Kay <superkay0101@gmail.com>
2025-04-10 16:26:36 +0800

    fix: 修正 CheckoutFormPage.jsx x 軸

--- src/pages/front/CheckoutFormPage.jsx
+++ src/pages/front/CheckoutFormPage.jsx
@@ -88,7 +88,7 @@ export default function CheckoutFormPage() {
   };
 
   return (
-    <div className="container">
+    <div className="container overflow-x-hidden">
       <ProgressBar currentStep={2} />
       <div className="row justify-content-center">
         <div className="col-md-10">
