96bb92c7c78a1a4508134f5f52bf2ae22f28c665
Kay <superkay0101@gmail.com>
2025-04-10 15:52:02 +0800

    fix: 修正 AboutPage.jsx x 軸問題

--- src/pages/front/AboutPage.jsx
+++ src/pages/front/AboutPage.jsx
@@ -89,7 +89,7 @@ export default function AboutPage() {
               <div className="col-md-6">
                 <img
                   className="w-100 object-fit-cover border-16 about-img"
-                  src="https://images.unsplash.com/photo-1486590868314-c6c18732d981?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3"
+                  src="https://images.unsplash.com/photo-1486590868314-c6c18732d981?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="使命與願景"
                 />
               </div>
